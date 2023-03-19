f347657c7bc8229457dc251f87b24308899deb0f
weaponsforge <forgeweapons@gmail.com>
2023-03-19 16:42:18 +0800

    chore: Update docs

--- client/src/lib/hooks/usetodo.js
+++ client/src/lib/hooks/usetodo.js
@@ -2,10 +2,21 @@ import { useSyncExternalStore } from 'react'
 
 // https://react.dev/reference/react/useSyncExternalStore
 
+// Global item id
 let nextId = 0
+
+// Global store containing an array of objects
 let todos = [{ id: nextId++, text: 'Todo #1' }]
+
+// Internal listeners for each method in todoStore initialized by useSyncExternalStore.
+// Needs to call emitChange() to take effect
 let listeners = []
 
+/**
+ * Exportable hook that uses useSyncExternalStore and a global variable to store data.
+ * Usage: const { todos, addTodo, deleteTodo } = useTodos()
+ * @returns {Object} { todos, addTodo, deleteTodo }
+ */
 export default function useTodos () {
   const todos = useSyncExternalStore(
     todoStore.subscribe,
@@ -20,6 +31,12 @@ export default function useTodos () {
   }
 }
 
+/**
+ * The external store with available useSyncExternalStore-required methods:
+ * subscribe() - Used and initialized internally by useSyncExternalStore
+ * getSnapshot() - Returns the current (full) snapshot of the global data variable
+ * getServerSnapshot() - Used in SSR
+ */
 export const todoStore = {
   addTodo () {
     todos = [ ...todos, { id: nextId++, text: 'Todo #' + nextId }]
